2435a17cc7052889dbf8b968e99df00895702bbe
pswami <pauras53@yahoo.com>
2022-04-06 22:14:01 -0700

    update contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -22,6 +22,8 @@ contract WavePortal {
 
   Wave[] waves;
 
+  mapping(address => uint256) public lastWavedAt;
+
   constructor() payable {
       console.log("We have been constructed!");
       /*
@@ -31,6 +33,13 @@ contract WavePortal {
   }
 
   function wave(string memory _message) public {
+      require(
+        lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+        "Wait 15m"
+      );
+
+      lastWavedAt[msg.sender] = block.timestamp;
+
       totalWaves += 1;
       console.log("%s has waved!", msg.sender);
 
